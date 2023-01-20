3e68b79c989363ad22509a2ffcabdaec30de350a
xjfdzn <xjfborges@gmail.com>
2023-01-20 20:44:55 -0300

    added day button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  swim: ["01-01", "01-02", "01-06"],
-  water: ["01-04", "01-05"],
-  fruit: ["01-01", "01-03"],
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if(dayExists) {
+    alert('❌ Dia já incluso ❌')
+    return
+  }
+
+  alert('Dia adicionado com sucesso ✅')
+  nlwSetup.addDay(today)
 }
 
+function save() {
+  localStorage.setItem('nlwSetup@habits', JSON.stringify(nlwSetup.data))
+}
+const data = JSON.parse(localStorage.getItem("nlwSetup@habits"))
 nlwSetup.setData(data)
 nlwSetup.load()
+
+
+
+// const data = {
+//   swim: ["01-01", "01-02", "01-06"],
+//   water: ["01-04", "01-05"],
+//   fruit: ["01-01", "01-03"],
+// }
+
